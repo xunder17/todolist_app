8aa179cb9c799a331e33a33b2f24b2a2d3404b2a
Was404 <wqerasmr@mail.ru>
2024-08-08 13:05:57 +0300

    setup loggining

--- run.py
+++ run.py
@@ -1,6 +1,26 @@
 from app import create_app
+import logging
 
 app = create_app()
 
+
+def setup_logging(log_file='app.log', level=logging.INFO):
+    """Настраивает логирование."""
+    logging.basicConfig(
+        filename=log_file,
+        filemode='a',  # Запись в конец файла
+        format='%(asctime)s - %(levelname)s - %(message)s',
+        level=level
+    )
+
+    
+"""Пример записи логов.
+    logging.debug("Это отладочное сообщение.")
+    logging.info("Это информационное сообщение.")
+    logging.warning("Это предупреждение.")
+    logging.error("Это сообщение об ошибке.")
+    logging.critical("Это критическое сообщение.")
+"""
 if __name__ == '__main__':
     app.run(debug=True)
+    setup_logging()
