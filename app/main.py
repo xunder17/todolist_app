441a17c7b54e40ee08213f185c5ec0f2f9c59ad3
Was404 <wqerasmr@mail.ru>
2024-08-08 13:42:21 +0300

    clean logger

--- app/main.py
+++ app/main.py
@@ -1,21 +1,9 @@
 from fastapi import FastAPI
 from .routers import users, tasks
-import logging
 
 app = FastAPI()
 
 app.include_router(users.router)
 app.include_router(tasks.router)
 
-
-def setup_logging(log_file='app.log', level=logging.INFO):  # TODO
-    """Настраивает логирование."""
-    logging.basicConfig(
-        filename=log_file,
-        filemode='a',  # Запись в конец файла
-        format='%(asctime)s - %(levelname)s - %(message)s',
-        level=level
-    )
-
-
-setup_logging()
+# uvicorn example:app --log-config /path/to/log.ini
--- run.py
+++ run.py
@@ -1,26 +1,6 @@
 from app import create_app  # cannot import name 'create_app' from 'app'
-import logging
 
 app = create_app()  # Error
 
-
-def setup_logging(log_file='app.log', level=logging.INFO):  # TODO
-    """Настраивает логирование."""
-    logging.basicConfig(
-        filename=log_file,
-        filemode='a',  # Запись в конец файла
-        format='%(asctime)s - %(levelname)s - %(message)s',
-        level=level
-    )
-
-
-"""Пример записи логов.
-    logging.debug("Это отладочное сообщение.")
-    logging.info("Это информационное сообщение.")
-    logging.warning("Это предупреждение.")
-    logging.error("Это сообщение об ошибке.")
-    logging.critical("Это критическое сообщение.")
-"""
 if __name__ == '__main__':
-    app.run(debug=True)
-    setup_logging()
+    app.run(debug=True) 
