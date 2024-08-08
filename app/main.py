a8d17db6f731e4d178479b150b14f514b8656e4c
Was404 <wqerasmr@mail.ru>
2024-08-08 13:25:15 +0300

    replace logginig

--- app/main.py
+++ app/main.py
@@ -1,7 +1,21 @@
 from fastapi import FastAPI
 from .routers import users, tasks
+import logging
 
 app = FastAPI()
 
 app.include_router(users.router)
 app.include_router(tasks.router)
+
+
+def setup_logging(log_file='app.log', level=logging.INFO):  # TODO
+    """Настраивает логирование."""
+    logging.basicConfig(
+        filename=log_file,
+        filemode='a',  # Запись в конец файла
+        format='%(asctime)s - %(levelname)s - %(message)s',
+        level=level
+    )
+
+
+setup_logging()
