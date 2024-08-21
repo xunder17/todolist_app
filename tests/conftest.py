7b7b812bbd07e88c475dea562720ab229eef8412
Was404 <wqerasmr@mail.ru>
2024-08-21 21:07:56 +0300

    test db modifield

--- tests/conftest.py
+++ tests/conftest.py
@@ -1,24 +1,18 @@
 from sqlalchemy import create_engine
 from sqlalchemy.orm import sessionmaker
 from app.database import Base
-import pytest
+
 
 # Создание тестовой базы данных
 TEST_DATABASE_URL = "sqlite:///./test.db"
 
 engine_test = create_engine(TEST_DATABASE_URL, 
                             connect_args={"check_same_thread": False})
+
 TestingSessionLocal = sessionmaker(autocommit=False, 
                                    autoflush=False, 
                                    bind=engine_test)
 
 
-@pytest.fixture(scope="function", autouse=True)
-def override_get_db():
+def init_db():
     Base.metadata.create_all(bind=engine_test)
-    db = TestingSessionLocal()
-    try:
-        yield db
-    finally:
-        db.close()
-        Base.metadata.drop_all(bind=engine_test)
